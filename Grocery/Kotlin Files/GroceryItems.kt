a111c1b9209b78d6e5fd481696c4cd928baf0a56
rushmi06 <111074592+rushmi06@users.noreply.github.com>
2022-09-25 00:04:02 +0530

    Create GroceryItems.kt

---
+++ Grocery/Kotlin Files/GroceryItems.kt
@@ -0,0 +1,22 @@
+package com.rahulpa.groceryapp
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "grocery_items")
+
+data class GroceryItems(
+
+    @ColumnInfo(name = "itemName")
+    var itemName: String,
+
+    @ColumnInfo(name = "itemQuantity")
+    var itemQuantity: Int,
+
+    @ColumnInfo(name = "itemPrice")
+    var itemPrice: Int,
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+}
