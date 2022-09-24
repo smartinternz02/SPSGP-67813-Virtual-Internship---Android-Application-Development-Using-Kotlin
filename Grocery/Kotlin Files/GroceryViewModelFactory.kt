c9d045624ad6103de423ec89f70d777842a0119e
rushmi06 <111074592+rushmi06@users.noreply.github.com>
2022-09-25 00:13:10 +0530

    Create GroceryViewModelFactory.kt

---
+++ Grocery/Kotlin Files/GroceryViewModelFactory.kt
@@ -0,0 +1,11 @@
+package com.rahulpa.groceryapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class GroceryViewModelFactory(private val repository: GroceryRepository): ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return GroceryViewModel(repository) as T
+    }
+}
